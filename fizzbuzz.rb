8dd312c08f704c2a4c8c6fc2d8abfad3cff5ad61
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-11-15 01:02:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-describe "fizzbuzz"
-  it 'returns "fizz" when the number is divisible by 3' do
-end
-end
-end
-
-def fizzbuzz(counter)
-  
-  counter += 1
-if counter %3 == 0 
-  puts "Fizz"
-elsif counter %5 == 0
-puts "FizzBuzz"
-end
